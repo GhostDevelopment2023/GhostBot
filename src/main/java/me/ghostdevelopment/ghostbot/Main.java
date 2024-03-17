c0e1b8fd47012c3488055dfd63bbc6c6d4103d46
GhostAndry <andrearonzoni20062022@gmail.com>
2024-03-17 22:12:04 +0100

    Save 17/03/2024 22:11

--- src/main/java/me/ghostdevelopment/ghostbot/Main.java
+++ src/main/java/me/ghostdevelopment/ghostbot/Main.java
@@ -16,27 +16,33 @@ import java.util.EventListener;
 public class Main extends Logs {
 
 	private static final ArrayList<EventListener> events = new ArrayList<>();
+
 	private static String TOKEN;
-	private static final String HELP_MSG = "Args: \n" +
-			"    --token|-t [discord bot token]";
 
 	public static void main(String[] args) {
 
-		if(args.length==0){
-			System.out.println(HELP_MSG);
-			return;
-		}
-		if ( args.length==2 && (args[0].equalsIgnoreCase("--token")||args[0].equalsIgnoreCase("-t"))) TOKEN = args[1];
-		else{
-			System.out.println(HELP_MSG);
+		if(args.length!=2 || args[1].isBlank() || args[1].isEmpty()){
+			log(Level.ERROR, "Canno't start discord bot: "+ Color.RED +"Token missing.");
 			return;
 		}
 
-		JDA jda = JDABuilder.createDefault(TOKEN)
-				.enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
-				.setStatus(OnlineStatus.DO_NOT_DISTURB)
-				.addEventListeners(events)
-				.build();
+		if(args[0].equalsIgnoreCase("--token") || args[0].equalsIgnoreCase("-t")){
+			TOKEN=args[1];
+		} else {
+			log(Level.ERROR, "java -jar jarname.jar --token <bot token>");
+			return;
+		}
+
+		try {
+			JDA jda = JDABuilder.createDefault(TOKEN)
+					.enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
+					.setStatus(OnlineStatus.DO_NOT_DISTURB)
+					.addEventListeners(events)
+					.build();
+		}catch (Exception e){
+			log(Level.ERROR, "Canno't start discord bot: "+ Color.RED +"Token not valid.");
+			return;
+		}
 
 		log(Level.LOG, Color.GREEN_BOLD+"Bot successfully started!");
 
@@ -44,16 +50,16 @@ public class Main extends Logs {
 
 	private void registerCommands() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 		String packageName = getClass().getPackage().getName();
-		for(Class<? extends GhostEvent> clazz: new Reflections(packageName + ".commands.commands").getSubTypesOf(GhostEvent.class)){
+		for(Class<? extends GhostEvent> clazz: new Reflections(packageName + ".commands.impl").getSubTypesOf(GhostEvent.class)){
 			GhostEvent command = null;
 
 			command = clazz.getDeclaredConstructor().newInstance();
 
 			try {
 
-				log(Level.LOG, "Loaded $command event.");
+				log(Level.LOG, "Loaded {0} event.");
 			} catch (Exception e) {
-				log(Level.ERROR, "Can't load $command event");
+				log(Level.ERROR, "Can't load {0} event");
 			}
 		}
 	}
